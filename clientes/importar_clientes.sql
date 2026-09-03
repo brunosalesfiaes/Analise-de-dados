a2c4d283abbf84e3652cbfbebb000f793f39e66c
brunosalesfiaes <128549718+brunosalesfiaes@users.noreply.github.com>
2026-09-03 13:22:59 -0300

    subindo alteracao de nome da pasta

--- clientes/importar_clientes.sql
+++ clientes/importar_clientes.sql
@@ -1,7 +1,7 @@
 USE Arrecadacao;
 SET NAMES utf8mb4;
 
-LOAD DATA INFILE 'C:/Users/BRUNO/OneDrive/Documentos/Desktop/Impresorajava/Analise de dados/dados/base_clientes_import.csv'
+LOAD DATA INFILE 'C:/Users/BRUNO/OneDrive/Documentos/Desktop/BigData/Analise de dados/dados/base_clientes_import.csv'
 INTO TABLE Clientes
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
